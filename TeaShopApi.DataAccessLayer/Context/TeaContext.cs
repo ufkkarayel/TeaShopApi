a200535ed0e2c666abc22de0e9296aad04c9f2a7
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-11-14 21:55:26 +0300

    Api yazıldı. Create ve update işlemleri eklendi.

--- TeaShopApi.DataAccessLayer/Context/TeaContext.cs
+++ TeaShopApi.DataAccessLayer/Context/TeaContext.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeaShopApi.EntityLayer.Concrete;
 
 namespace TeaShopApi.DataAccessLayer.Context
 {
-    public class TeaContext
+    public class TeaContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; initial catalog=TeaShopDb; integrated security=true;");
+        }
+        public DbSet<Drink> Drinks{ get; set; }
+        public DbSet<Message> Messages{ get; set; }
+        public DbSet<Question> Questions{ get; set; }
+        public DbSet<Testimonial> Testimonials{ get; set; }
 
     }
 }
---
+++ TeaShopApi/Controllers/DrinksController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TeaShopApi.BusinessLayer.Abstract;
+using TeaShopApi.DataAccessLayer.Abstract;
+using TeaShopApi.DtoLayer.DrinkDtos;
+using TeaShopApi.EntityLayer.Concrete;
+
+namespace TeaShopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DrinksController : ControllerBase
+    {
+        private readonly IDrinkService _drinkService;
+
+        public DrinksController(IDrinkService drinkService)
+        {
+            _drinkService = drinkService;
+        }
+
+        [HttpGet]
+        public IActionResult DrinkList()
+        {
+            var values = _drinkService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateDrink(CreateDrinkDto createdDrinkDto)
+        {
+
+            Drink drink = new Drink()
+            {
+                DrinkImageUrl = createdDrinkDto.DrinkImageUrl,
+                DrinkName = createdDrinkDto.DrinkName,
+                DrinkPrice = createdDrinkDto.DrinkPrice,
+            };
+            _drinkService.TInsert(drink);
+            return Ok(drink);
+        }
+    }
+}
