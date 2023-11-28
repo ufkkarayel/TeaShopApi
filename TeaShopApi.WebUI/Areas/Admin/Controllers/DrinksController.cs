255dc70ceec3f077f62f0197373cda688ca78595
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-11-28 22:27:59 +0300

    gün sonu

---
+++ TeaShopApi.WebUI/Areas/Admin/Controllers/DrinksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeaShopApi.WebUI.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class DrinksController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
