2efba7328a59960cf248c9f79c2e0c1f5238d512
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-11-30 20:21:13 +0300

    drink controller eklendi

--- TeaShopApi.WebUI/Areas/Admin/Controllers/DrinksController.cs
+++ TeaShopApi.WebUI/Areas/Admin/Controllers/DrinksController.cs
@@ -5,8 +5,21 @@ namespace TeaShopApi.WebUI.Areas.Admin.Controllers
 	[Area("Admin")]
 	public class DrinksController : Controller
 	{
-		public IActionResult Index()
+		private readonly IHttpClientFactory _httpClientFactory;
+
+        public DrinksController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
 		{
+			var client=_httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7263/api/Drinks");
+			if (responseMessage != null)
+			{
+
+			}
 			return View();
 		}
 	}
