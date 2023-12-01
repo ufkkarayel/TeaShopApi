80e5e3d887e80df1fcfd7423c9e6172bf2a408fd
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-01 22:13:35 +0300

    choose işlemleri eklendi

---
+++ TeaShopApi.WebUI/ViewComponents/_DefaultBookingComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeaShopApi.WebUI.ViewComponents
+{
+    public class _DefaultBookingComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultBookingComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
