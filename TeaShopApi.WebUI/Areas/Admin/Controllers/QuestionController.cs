15736801856b806432f00b1ef6a916a6aed733d7
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-01 20:24:33 +0300

    question listelemesi yapıldı

---
+++ TeaShopApi.WebUI/Areas/Admin/Controllers/QuestionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TeaShopApi.WebUI.Dtos.QuestionDtos;
+
+namespace TeaShopApi.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class QuestionController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public QuestionController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client =_httpClientFactory.CreateClient();
+            var responseMessage=await client.GetAsync("https://localhost:7263/api/Questions");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultQuestionDto>>(jsonData); 
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
