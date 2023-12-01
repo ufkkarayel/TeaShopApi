9adb6ec59ed8a60ec955c5a4b1aa8bc5e6ad1185
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-01 22:05:08 +0300

    about işlemleri yapıldı

---
+++ TeaShopApi.WebUI/ViewComponents/_DefaultAboutComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeaShopApi.WebUI.ViewComponents
+{
+    public class _DefaultAboutComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultAboutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
