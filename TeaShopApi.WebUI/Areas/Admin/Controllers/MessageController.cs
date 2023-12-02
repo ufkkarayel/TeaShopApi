4e7bf780817c34fe58e02baf0e9a42d8cbca0ed5
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-02 15:48:13 +0300

    message contoller ve testimonial controller eklendi

---
+++ TeaShopApi.WebUI/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TeaShopApi.WebUI.Dtos.MessageDtos;
+
+namespace TeaShopApi.WebUI.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class MessageController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MessageController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7263/api/Messages");
+            if (responseMessage.IsSuccessStatusCode) 
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMessageDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ TeaShopApi.WebUI/Areas/Admin/Controllers/TestimonialController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TeaShopApi.WebUI.Dtos.TestimonialDtos;
+
+namespace TeaShopApi.WebUI.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class TestimonialController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestimonialController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7263/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
