e83419a73d67425c25f970ebca16901c0daea392
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-02 14:30:12 +0300

    clientscomponent eklendi

---
+++ TeaShopApi.WebUI/ViewComponents/_DefaultClientComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeaShopApi.WebUI.ViewComponents
+{
+    public class _DefaultClientComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultClientComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
