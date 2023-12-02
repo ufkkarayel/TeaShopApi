672b48ca5fc28b1289a3823aac09b92342978d6d
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-02 17:57:05 +0300

    update işlemleri eklendi

--- TeaShopApi.WebUI/Areas/Admin/Controllers/MessageController.cs
+++ TeaShopApi.WebUI/Areas/Admin/Controllers/MessageController.cs
@@ -47,5 +47,30 @@ namespace TeaShopApi.WebUI.Areas.Admin.Controllers
             }
             return View();
         }
+        public async Task<IActionResult> DeleteMessage(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7263/api/Messages?id="+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateMessage(int id)
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7263/api/Messages?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<UpdateMessageDto>(jsonData);
+                return View(values);
+
+            }
+            return View();
+
+        }
     }
 }
--- TeaShopApi.WebUI/Areas/Admin/Controllers/TestimonialController.cs
+++ TeaShopApi.WebUI/Areas/Admin/Controllers/TestimonialController.cs
@@ -48,5 +48,28 @@ namespace TeaShopApi.WebUI.Areas.Admin.Controllers
             return View();
 
         }
+        public async Task<IActionResult> DeleteTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7263/api/Testimonial?id="+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7263/api/Testimonial?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<UpdateTestimonialDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
     }
 }
