c299d9876481e4ecbeae249034db429057486da6
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-02 15:37:11 +0300

    messagedto ve testimonialdto oluşturuldu.

---
+++ TeaShopApi.WebUI/Dtos/MessageDtos/CreateMessageDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TeaShopApi.WebUI.Dtos.MessageDtos
+{
+    public class CreateMessageDto
+    {      
+        public string SenderName { get; set; }
+        public string MessageSubject { get; set; }
+        public string MessageEmail { get; set; }
+        public string MessageDetail { get; set; }
+        public DateTime MessageSendDate { get; set; }
+    }
+}
---
+++ TeaShopApi.WebUI/Dtos/MessageDtos/UpdateMessageDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TeaShopApi.WebUI.Dtos.MessageDtos
+{
+    public class UpdateMessageDto
+    {
+        public int MessageID { get; set; }
+        public string SenderName { get; set; }
+        public string MessageSubject { get; set; }
+        public string MessageEmail { get; set; }
+        public string MessageDetail { get; set; }
+        public DateTime MessageSendDate { get; set; }
+    }
+}
