1713980d929a0f65aad329e4bcedba208df207d1
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-02 22:07:10 +0300

    gün sonu

--- TeaShopApi.WebUI/ViewComponents/_DefaultClientComponentPartial.cs
+++ TeaShopApi.WebUI/ViewComponents/_DefaultClientComponentPartial.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TeaShopApi.WebUI.Dtos.DrinkDto;
+using TeaShopApi.WebUI.Dtos.TestimonialDtos;
 
 namespace TeaShopApi.WebUI.ViewComponents
 {
@@ -12,7 +16,29 @@ namespace TeaShopApi.WebUI.ViewComponents
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7263/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
 }
+
+
+//public async Task<IViewComponentResult> InvokeAsync()
+//{
+//    var client = _httpClientFactory.CreateClient();
+//    var responseMessage = await client.GetAsync("https://localhost:7263/api/Drinks/");
+//    if (responseMessage.IsSuccessStatusCode)
+//    {
+//        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+//        var values = JsonConvert.DeserializeObject<List<ResultDrinkDto>>(jsonData);
+//        return View(values);
+//    }
+//    return View();
+//}
