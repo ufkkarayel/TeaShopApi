482069039bdc06ca1bba3e6325287bbe89ff0dc1
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-02 14:35:29 +0300

    contact partial eklendi

---
+++ TeaShopApi.WebUI/ViewComponents/_DefaultContactComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeaShopApi.WebUI.ViewComponents
+{
+    public class _DefaultContactComponentPartial :ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultContactComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
